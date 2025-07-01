df0f0b7042db79ede671692ddf08bf8c2fbf6978
MahaGit1233 <maharush5409@gmail.com>
2025-07-01 19:41:34 +0530

    Creating middleware commit

--- app.js
+++ app.js
@@ -2,9 +2,21 @@ const express = require("express");
 
 const app = express();
 
-app.use((req,res,next)=>{
-    console.log('Server is up and running on port 3000! Ready to handle requests.');
-    res.send('<h1>Hello World</h1>');
+app.use((req, res, next) => {
+  req.user = "Guest";
+  console.log('new request property set');
+  next();
+});
+
+app.use('/welcome',(req,res,next)=>{
+    console.log('welcome page');
+    next();
 })
 
-app.listen(3000);
+app.get('/welcome',(req,res,next)=>{
+    res.send(`<h1>Welcome ${req.user}</h1>`);
+})
+
+app.listen(3000,()=>{
+    console.log('server is running');
+});
