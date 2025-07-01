42b08a364098fe93845e8c92e36b38b33d38f7ed
MahaGit1233 <maharush5409@gmail.com>
2025-07-01 20:22:38 +0530

    question 1 commit

--- app.js
+++ app.js
@@ -3,20 +3,40 @@ const express = require("express");
 const app = express();
 
 app.use((req, res, next) => {
-  req.user = "Guest";
-  console.log('new request property set');
+  console.log("middleware 1 created");
   next();
 });
 
-app.use('/welcome',(req,res,next)=>{
-    console.log('welcome page');
-    next();
-})
+app.get("/", (req, res, next) => {
+  res.send("<h1>Home Page</h1>");
+});
 
-app.get('/welcome',(req,res,next)=>{
-    res.send(`<h1>Welcome ${req.user}</h1>`);
-})
+app.use("/orders", (req, res, next) => {
+  console.log("orders page");
+  next();
+});
 
-app.listen(3000,()=>{
-    console.log('server is running');
+app.get("/orders", (req, res, next) => {
+  res.send("<h1>Here is the list of all orders.</h1>");
+});
+
+app.post("/orders", (req, res, next) => {
+  res.send("<h1>A new order has been created.</h1>");
+});
+
+app.use("/users", (req, res, next) => {
+  console.log("users page");
+  next();
+});
+
+app.get("/users", (req, res, next) => {
+  res.send("<h1>Here is the list of all users.</h1>");
+});
+
+app.post("/users", (req, res, next) => {
+  res.send("<h1>A new user has been added.</h1>");
+});
+
+app.listen(3000, () => {
+  console.log("server is running on http://localhost:3000");
 });
