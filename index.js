b736327143dde4b7997c8988790a7c7fa2b46285
MahaGit1233 <maharush5409@gmail.com>
2025-07-01 21:03:32 +0530

    question 2 unknown route commit

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require("express");
+
+const app = express();
+
+app.use((req, res, next) => {
+  console.log("middleware 1 created");
+  next();
+});
+
+app.get("/", (req, res, next) => {
+  res.send("<h1>Home Page</h1>");
+});
+
+app.use("/products", (req, res, next) => {
+  console.log("products page");
+  next();
+});
+
+app.get("/products", (req, res, next) => {
+  res.send("<h1>Here is the list of all products.</h1>");
+});
+
+app.post("/products", (req, res, next) => {
+  res.send("<h1>A new product has been added.</h1>");
+});
+
+app.use("/categories", (req, res, next) => {
+  console.log("categories page");
+  next();
+});
+
+app.get("/categories", (req, res, next) => {
+  res.send("<h1>Here is the list of all categories.</h1>");
+});
+
+app.post("/categories", (req, res, next) => {
+  res.send("<h1>A new category has been created.</h1>");
+});
+
+app.use(/.*/, (req, res) => {
+  res.status(404).send("<h1>404 - Page Not Found</h1>");
+});
+
+app.listen(4000, () => {
+  console.log("server is running on http://localhost:4000");
+});
