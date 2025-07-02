20313bf51785e158ee502d573931e102f9d00db4
MahaGit1233 <maharush5409@gmail.com>
2025-07-02 15:32:31 +0530

    dynamic routes commit

--- app.js
+++ app.js
@@ -7,35 +7,15 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get("/", (req, res, next) => {
+app.get("/", (req, res) => {
   res.send("<h1>Home Page</h1>");
 });
 
-app.use("/orders", (req, res, next) => {
-  console.log("orders page");
-  next();
-});
-
-app.get("/orders", (req, res, next) => {
-  res.send("<h1>Here is the list of all orders.</h1>");
-});
-
-app.post("/orders", (req, res, next) => {
-  res.send("<h1>A new order has been created.</h1>");
-});
-
-app.use("/users", (req, res, next) => {
-  console.log("users page");
-  next();
-});
-
-app.get("/users", (req, res, next) => {
-  res.send("<h1>Here is the list of all users.</h1>");
-});
-
-app.post("/users", (req, res, next) => {
-  res.send("<h1>A new user has been added.</h1>");
-});
+app.get("/welcome/:username",(req,res)=>{
+  const name=req.params.username;
+  const role=req.query.role;
+  res.send(`<h1>Welcome ${name}, your role is ${role}.</h1>`);
+})
 
 app.listen(3000, () => {
   console.log("server is running on http://localhost:3000");
