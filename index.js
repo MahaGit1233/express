b8af40b5f26a0017ed4b458539f917998d31927a
MahaGit1233 <maharush5409@gmail.com>
2025-07-02 20:12:29 +0530

    router commit

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require("express");
 
 const app = express();
+const categoriesRouter = require("./routes/categories");
+const productsRouter = require("./routes/products");
 
-app.use((req, res, next) => {
-  const method = req.method;
-  const endpoint = req.url;
-  console.log(`${method} request made to ${endpoint}.`);
-  next();
-});
+// app.use((req, res, next) => {
+//   const method = req.method;
+//   const endpoint = req.url;
+//   console.log(`${method} request made to ${endpoint}.`);
+//   next();
+// });
 
 app.use((req, res, next) => {
   console.log("middleware 1 created");
@@ -18,31 +20,9 @@ app.get("/", (req, res) => {
   res.send("<h1>Home Page</h1>");
 });
 
-app.use("/products", (req, res, next) => {
-  console.log("products page");
-  next();
-});
+app.use("/products", productsRouter);
 
-app.get("/products", (req, res) => {
-  res.send("<h1>Here is the list of all products.</h1>");
-});
-
-app.post("/products", (req, res) => {
-  res.send("<h1>A new product has been added.</h1>");
-});
-
-app.use("/categories", (req, res, next) => {
-  console.log("categories page");
-  next();
-});
-
-app.get("/categories", (req, res) => {
-  res.send("<h1>Here is the list of all categories.</h1>");
-});
-
-app.post("/categories", (req, res) => {
-  res.send("<h1>A new category has been created.</h1>");
-});
+app.use("/categories", categoriesRouter);
 
 app.use(/.*/, (req, res) => {
   res.status(404).send("<h1>404 - Page Not Found</h1>");
