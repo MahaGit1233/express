ddfe59b6045216031af0d3308ea32b97d0d0a2a6
MahaGit1233 <maharush5409@gmail.com>
2025-07-04 21:28:39 +0530

    centralised error handling commit

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -1,11 +1,35 @@
+const { sendErrorResponse } = require("../utils/response");
+
 const getCartForUser = (req, res) => {
-  res.send(`<h1>Fetching cart for user with ID: ${req.params.userId}</h1>`);
+  try {
+    let userId = Number(req.params.userId);
+
+    if (userId > 3) {
+      let err = new Error("User not found");
+      err.statusCode = 404;
+      throw err;
+    }
+
+    res.send(`<h1>Fetching cart for user with ID: ${userId}</h1>`);
+  } catch (err) {
+    sendErrorResponse(res, err);
+  }
 };
 
 const addProductToUser = (req, res) => {
-  res.send(
-    `<h1>Adding product to cart for user with ID: ${req.params.userId}</h1>`
-  );
+  try {
+    let userId = Number(req.params.userId);
+
+    if (userId > 3) {
+      let err = new Error("User not found");
+      err.statusCode = 404;
+      throw err;
+    }
+
+    res.send(`<h1>Adding product to cart for user with ID: ${userId}</h1>`);
+  } catch (err) {
+    sendErrorResponse(res, err);
+  }
 };
 
 module.exports = {
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,23 @@
+const { sendErrorResponse } = require("../utils/response");
+
 const getAllUsers = (req, res) => {
   res.send(`<h1>Fetching all users</h1>`);
 };
 
 const getUserById = (req, res) => {
-  res.send(`<h1>Fetching user with ID: ${req.params.id}</h1>`);
+  try {
+    let userId = Number(req.params.id);
+
+    if (userId > 3) {
+      let err = new Error("User not found");
+      err.statusCode = 404;
+      throw err;
+    }
+
+    res.send(`<h1>Fetching user with ID: ${userId}</h1>`);
+  } catch (err) {
+    return sendErrorResponse(res, err);
+  }
 };
 
 const addUser = (req, res) => {
